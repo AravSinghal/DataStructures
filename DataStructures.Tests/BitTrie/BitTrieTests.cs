5a6f9972055f7731904dd5c9d3571dcd1905b185
AravSinghal <singhal.arav@gmail.com>
2016-05-14 14:29:12 +0530

    Added Contains method to BitTrie with tests.

--- DataStructures.Tests/BitTrie/BitTrieTests.cs
+++ DataStructures.Tests/BitTrie/BitTrieTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DataStructures.Tests
 {
@@ -11,12 +12,22 @@ namespace DataStructures.Tests
             ushort expected = 0x6;
 
             BitTrie trie = new BitTrie(3);
-            trie.Insert(0x2);
-            trie.Insert(0x7);
+            trie.InsertRange(0x2, 0x7);
 
             ushort actual = trie.MaximizeXor(0x4);
-            
+
             Assert.AreEqual(expected, actual, $"Wrong value returned: {actual} instead of {expected}");
+        }
+
+        [TestMethod]
+        public void ContainsTest()
+        {
+            BitTrie trie = new BitTrie(3);
+            trie.InsertRange(0x2, 0x5, 0x7);
+
+            Assert.IsTrue(trie.Contains(0x5), $"Trie contains {0x5} but returned {false}");
+
+            Assert.IsFalse(trie.Contains(0x6), $"Trie doesn't contain {0x6} but returned {true}");
         }
     }
 }
--- DataStructures/BitTrie/BitTrie.cs
+++ DataStructures/BitTrie/BitTrie.cs
@@ -13,26 +13,43 @@ namespace DataStructures
             public Node[] children = new Node[2];
         }
 
-        private Node root = new Node();
+        private readonly Node root = new Node();
 
         /// <summary>
-        /// The number of bits in the maximum number that will be inserted into the tree.
+        /// The number of bits in the maximum value that will be inserted in the <see cref="BitTrie"/>.
         /// </summary>
         private readonly byte maxDepth;
 
+        /// <summary>
+        /// Create a new instance of <see cref="BitTrie"/>.
+        /// </summary>
+        /// <param name="maxDepth">The number of bits in the maximum value that will be inserted in the <see cref="BitTrie"/>. Defaults to sizeof(<see cref="ushort"/>) * 8.</param>
         public BitTrie(byte maxDepth = sizeof(ushort) * 8)
         {
             this.maxDepth = maxDepth;
         }
 
+        /// <summary>
+        /// Inserts all the numbers in <paramref name="numbers"/> in the <see cref="BitTrie"/>.
+        /// </summary>
+        /// <param name="numbers">The numbers to be inserted.</param>
+        public void InsertRange(params ushort[] numbers)
+        {
+            foreach (ushort number in numbers)
+                Insert(number);
+        }
+
+        /// <summary>
+        /// Insert a <paramref name="number"/> in the <see cref="BitTrie"/>.
+        /// </summary>
+        /// <param name="number">The number to be inserted.</param>
         public void Insert(ushort number)
         {
             Node node = root;
 
             for (byte d = maxDepth; d > 0; d--)
             {
-                ushort mask = (ushort)(1 << (d - 1));
-                ushort bit = (ushort)((uint)(mask & number) >> (d - 1));
+                uint bit = GetBit(number, d);
 
                 if (node.children[bit] == null)
                     node.children[bit] = new Node();
@@ -41,6 +58,34 @@ namespace DataStructures
             }
         }
 
+        /// <summary>
+        /// Determines whether <paramref name="number"/> is in the <see cref="BitTrie"/>.
+        /// </summary>
+        /// <param name="number">The number to be located in the <see cref="BitTrie"/>.</param>
+        /// <returns><code>true</code> if <paramref name="number"/> is in the <see cref="BitTrie"/> else returns <code>false</code>.</returns>
+        public bool Contains(ushort number)
+        {
+            Node node = root;
+
+            for (byte d = maxDepth; d > 0; d--)
+            {
+                uint bit = GetBit(number, d);
+
+                // The branch we need doesn't exist; therefore the number is not in the tree.
+                if (node.children[bit] == null)
+                    return false;
+
+                node = node.children[bit];
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns the maximum value after performing the XOR operation with <paramref name="q"/> and all the values in the <see cref="BitTrie"/>.
+        /// </summary>
+        /// <param name="q">The value to XOR the values in the <see cref="BitTrie"/> with.</param>
+        /// <returns>The maximum XOR result.</returns>
         public ushort MaximizeXor(ushort q)
         {
             Node node = root;
@@ -48,8 +93,7 @@ namespace DataStructures
 
             for (byte d = maxDepth; d > 0; d--)
             {
-                ushort mask = (ushort)(1 << (d - 1));
-                ushort bit = (ushort)((uint)(mask & q) >> (d - 1));
+                uint bit = GetBit(q, d);
 
                 // We always assume that the maximum XOR of values at this bit is 1 at first
                 ushort val = 1; // Maximum value to be assigned to XOR result at bit
@@ -57,15 +101,27 @@ namespace DataStructures
 
                 if (node.children[bit] == null)
                 {
-                    bit = (ushort)(1 - bit); // Toggle bit
+                    bit = 1 - bit; // Toggle bit
                     val = 0;
                 }
 
-                max += (ushort)(val << (d - 1));
+                max |= (ushort)(val << (d - 1));
                 node = node.children[bit];
             }
 
             return max;
+        }
+
+        /// <summary>
+        /// Helper method which returns the <paramref name="i"/> th bit (starting from LSB) of <paramref name="n"/>.
+        /// </summary>
+        /// <param name="n">The number whose bit is to be extracted.</param>
+        /// <param name="i">The 1 based (from LSB to MSB) index of the bit to be extracted.</param>
+        /// <returns>The extracted bit.</returns>
+        private static uint GetBit(ushort n, byte i)
+        {
+            ushort mask = (ushort)(1 << (i - 1));
+            return ((uint)(mask & n) >> (i - 1));
         }
     }
 }
